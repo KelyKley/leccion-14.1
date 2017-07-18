bdd4adadbbadd7664c3dc03fc79c3bca208c508d
kelyanamuro <kelyanamurohuamani@gmail.com>
2017-07-18 14:46:13 -0500

    Ejercicios evaluados terminados

--- assets3/js/main.js
+++ assets3/js/main.js
@@ -1,10 +1,6 @@
-function convertirCelsius(grados){
-	var x;
-    if (grados == "C") {
-        x = document.getElementById("c").value * 9 / 5 + 32;
-        document.getElementById("f").value = Math.round(x);
-    } else {
-        x = (document.getElementById("f").value -32) * 5 / 9;
-        document.getElementById("c").value = Math.round(x);
-    }
+function convertirCelcius(){
+	var celcius = document.getElementById("c").value;
+ 
+    rpta.innerText = (celcius*9)/5+32;
+
 }
