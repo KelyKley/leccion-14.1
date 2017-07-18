2cc10f0080118ab98e5bbe16bb83e91de6b892f5
kelyanamuro <kelyanamurohuamani@gmail.com>
2017-07-18 12:19:47 -0500

    Ejercicios evaluados en proceso

---
+++ assets3/js/main.js
@@ -0,0 +1,10 @@
+function convertirCelsius(grados){
+	var x;
+    if (grados == "C") {
+        x = document.getElementById("c").value * 9 / 5 + 32;
+        document.getElementById("f").value = Math.round(x);
+    } else {
+        x = (document.getElementById("f").value -32) * 5 / 9;
+        document.getElementById("c").value = Math.round(x);
+    }
+}
